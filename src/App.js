ee492f8719efe2f9d237dd63fd13490d26827a70
Daniel Meneses <daniel.meneses.10@gmail.com>
2023-03-30 11:11:01 -0500

    add-first-component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/images/logo.svg';
+import './assets/css/App.css';
+import MyComponent from './components/MyComponent';
+
+function HolaMundo(nombre) {
+  var presentacion = (
+    <h2>Hola soy {nombre}</h2>
+  );
+  return presentacion;
+}
+
 
 function App() {
+  var nombre = "Daniel";
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Bienvenido al Proyecto de Dani
-        </p>        
+        </p>
+        {HolaMundo(nombre)}
+        <section className='components'>
+        <MyComponent />
+      </section>
       </header>
+
+
     </div>
   );
 }
