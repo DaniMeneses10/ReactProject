49b526cac1b257aa9c11a4cafda261cdbff83fb7
Daniel Meneses <daniel.meneses.10@gmail.com>
2023-04-11 12:23:59 -0500

    add maquetacion folder

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Bowls from './components/Bowls'
 import Header from './components/Header';
 import Slider from './components/Slider';
 import Sidebar from './components/Sidebar';
+import Footer from './components/Footer';
 
 function HolaMundo(nombre) {
   var presentacion = (
@@ -40,9 +41,9 @@ function App() {
         </section>
 
         <Sidebar />
-      </div>
-
-
+        <div className='clearfix'></div>
+      </div>{/*End div center */}
+      <Footer/>
     </div>
   );
 }
